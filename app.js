b84f0b69bb1187163c75a3a3e0279dd89a89c7b0
mahesa kahfi <mahesakahfi2609@gmail.com>
2022-12-05 23:34:39 +0700

    setup

---
+++ app.js
@@ -0,0 +1,20 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
