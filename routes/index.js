48d0f51e708b0f5fe73c5d842d555cd30f3c494c
Sony MelliFriady <zoenasun@gmail.com>
2022-12-06 18:41:08 +0700

    [update] routes, create private routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 
 const publicRouter = require("./public");
+const privateRouter = require("./private");
 
 router.use("/public", publicRouter);
+router.use("/private", privateRouter);
+
 module.exports = router;
