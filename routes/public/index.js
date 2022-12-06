830ea44ed5189c0b191fc9efa0af544a55ebc5c7
Sony MelliFriady <zoenasun@gmail.com>
2022-12-06 17:17:49 +0700

    [update] routes public

--- routes/public/index.js
+++ routes/public/index.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const Auth = require("../../controllers/auth");
 
 router.post("/register", Auth.registerUser);
+router.post("/login", Auth.loginUser);
 
 module.exports = router;
