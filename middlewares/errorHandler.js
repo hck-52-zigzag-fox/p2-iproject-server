a9133eea889a9082bbffe28fc68da0d7efdcd892
Sony MelliFriady <zoenasun@gmail.com>
2022-12-06 16:36:08 +0700

    [create] middleware errorHandler

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,36 @@
+module.exports = {
+  errorHandler: (err, req, res, next) => {
+    let code;
+    let msg;
+    if (err.name === "SequelizeUniqueConstraintError") {
+      err.errors?.forEach((m) => {
+        msg = m.message;
+      });
+      code = 400;
+    } else if (err.name === "SequelizeValidationError") {
+      err.errors?.forEach((m) => {
+        msg = m.message;
+      });
+      code = 400;
+    } else if (err.name === "SequelizeDatabaseError") {
+      err.errors?.forEach((m) => {
+        msg = m.message;
+      });
+      code = 400;
+    } else if (err.name === "JsonWebTokenError") {
+      msg = err.message || err.name.message;
+      code = 401;
+    } else if (err.name === "ConflictCreateUser") {
+      msg = "username or email already registered";
+      code = 400;
+    } else {
+      msg = "internal server error";
+      code = 500;
+    }
+
+    res.status(code).json({
+      code: code,
+      message: msg,
+    });
+  },
+};
