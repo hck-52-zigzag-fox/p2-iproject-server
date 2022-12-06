5091884063e4b343373055e612c580136e315296
Sony MelliFriady <zoenasun@gmail.com>
2022-12-07 05:00:33 +0700

    [update] upload helpers

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const multer = require("multer");
 
 const hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
@@ -16,9 +17,37 @@ const jwtSign = (payload, secret) => {
 const jwtVerify = (payload, secret) => {
   return jwt.verify(payload, secret);
 };
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const filterFile = (req, file, cb) => {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: filterFile,
+});
+
 module.exports = {
   hashPassword,
   comparePassword,
   jwtSign,
   jwtVerify,
+  upload,
 };
