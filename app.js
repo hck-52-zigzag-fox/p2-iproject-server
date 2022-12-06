94e0f61b005aa18d817a7214426ef7cfaeaf1aed
Nadya <nadya.rere@gmail.com>
2022-12-06 11:52:27 +0700

    setup app

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+if(process.env.NODE_ENV !== "production"){
+    require("dotenv").config();
+  }
+  
+  const express = require("express");
+  const cors = require('cors')
+  const app = express();
+  const port = process.env.PORT || 100;
+  
+  const router = require("./routes");
+  const { errorHandler } = require("./middlewares/errorHandler");
+  
+  app.use(cors())
+  
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+  
+  app.use("/", router);
+  app.use(errorHandler);
+  
+  app.listen(port, (req, res) => {
+    console.log(`listening on port: ${port}`);
+  });
+  
