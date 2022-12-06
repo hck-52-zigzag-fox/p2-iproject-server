e98dd6a6b69b65cc844439593db266f297e9df1d
Rahmat <rahmathikaru03@gmail.com>
2022-12-06 14:11:12 +0700

    initiation setup project

---
+++ models/user.js
@@ -0,0 +1,31 @@
+"use strict";
+const { Model } = require("sequelize");
+const { hashPassword } = require("../helpers/bcrypt");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasOne(models.Profile);
+    }
+  }
+  User.init(
+    {
+      username: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  User.beforeCreate((instance) => {
+    instance.password = hashPassword(instance.password);
+  });
+  return User;
+};
