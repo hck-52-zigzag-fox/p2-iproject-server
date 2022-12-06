11da91d048632f899204fd90b38d06ff4fb5165d
Sony MelliFriady <zoenasun@gmail.com>
2022-12-06 17:15:37 +0700

    [update] loginUser

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { User } = require("../models");
 const { Op } = require("sequelize");
+const { comparePassword, jwtSign } = require("../helpers");
 
 class Auth {
   static async registerUser(req, res, next) {
@@ -18,6 +19,36 @@ class Auth {
       });
       if (!created) throw { name: "ConflictCreateUser" };
       res.status(201).json({ user });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async loginUser(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      if (!email) throw { name: "NoEmail" };
+      if (!password) throw { name: "NoPassword" };
+
+      let options = {
+        where: {
+          email,
+        },
+      };
+      const dataUser = await User.findOne(options);
+      if (!dataUser) throw { name: "ConflictLoginUser" };
+
+      if (dataUser.password) {
+        const isValidPwd = comparePassword(password, dataUser.password);
+        if (!isValidPwd) throw { name: "ConflictLoginUser" };
+
+        const generateToken = jwtSign({ id: dataUser.id }, process.env.JWT_SECRET);
+        res.status(200).json({
+          statusCode: 200,
+          access_token: generateToken,
+        });
+      }
     } catch (err) {
       next(err);
     }
