332dac73acde260780f5482de8d06eb865710108
Nadya <nadya.rere@gmail.com>
2022-12-06 17:28:39 +0700

    - fix: router patch - add: authorization

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const ItemController = require("../controllers/itemController");
 const OrderController = require("../controllers/orderControllers");
 const UserController = require("../controllers/userController");
 const { isLogin } = require("../middlewares/authentication");
+const { isAdmin } = require("../middlewares/authorization");
 
 const router = require("express").Router();
 
@@ -12,6 +13,5 @@ router.get("/items", ItemController.fetchItems);
 router.get("/orders", OrderController.fetchOrder);
 router.post("/orders", OrderController.addOrder);
 router.put("/orders/:id", OrderController.editOrder);
-router.patch("orders/:id", OrderController.changeStatusToPaid)
-
+router.patch("/orders/:id", isAdmin, OrderController.changeStatusToPaid)
 module.exports = router;
