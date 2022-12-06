9555629df9197d120362e30396427238473474f4
aniisah <anisaayu735@gmail.com>
2022-12-06 13:48:49 +0700

    fetch product

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const userRoute = require('./users')
 const productRoute = require('./product')
+const usersProduct = require('./usersProduct')
 
 router.use('/users', userRoute)
 router.use('/product', productRoute)
+router.use('/userProduct', usersProduct)
 
 
 module.exports = router
