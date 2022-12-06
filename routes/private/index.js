1dd999671d6fb1dfd0101cc302a7f56e8c2e5c9c
Sony MelliFriady <zoenasun@gmail.com>
2022-12-07 05:03:37 +0700

    [update] index routes

--- routes/private/index.js
+++ routes/private/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const mentorRouter = require("./mentor");
+const courseRouter = require("./course");
 
 router.use("/mentors", mentorRouter);
+router.use("/courses", courseRouter);
 
 module.exports = router;
