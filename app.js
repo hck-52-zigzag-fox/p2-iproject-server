2b3c5005c7cd5a55f4e49a4a8f1a6e925dcbc9c0
januaricks <muhammadjanu30@gmail.com>
2022-12-07 13:22:32 +0700

    setup error handling

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const cors = require("cors");
 const express = require("express");
+const { errorHandler } = require("./middlewares/errorHandling");
 const app = express();
-const port = 3000;
+const port = 9090;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+const { hashPassword } = require("../helpers/password");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -13,14 +12,45 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    phoneNumber: DataTypes.STRING,
-    address: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+          msg: `Email must be unique`,
+        },
+        validate: {
+          notEmpty: {
+            msg: `Email is a required`,
+          },
+          notNull: {
+            msg: `Email is a required`,
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: `Password is a required`,
+          },
+          notNull: {
+            msg: `Password is a required`,
+          },
+        },
+      },
+      phoneNumber: DataTypes.STRING,
+      address: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  User.beforeCreate((user) => {
+    return (user.password = hashPassword(user.password));
   });
   return User;
 };
