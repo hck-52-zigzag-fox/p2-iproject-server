fa1c57ddad988614a3db469bfc1550861672c360
atrsxvr <atrasx@gmail.com>
2022-12-07 14:24:59 +0700

    register done

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,5 @@
 const { User, Contract } = require("../models");
+const { comparePassword, signToken } = require("../helpers/helper");
 
 class UserController {
   static async register(req, res, next) {
@@ -13,6 +14,42 @@ class UserController {
       res.status(201).json({ id: user.id, email: user.email });
     } catch (err) {
         next(err)
+    }
+  }
+  static async login(req, res, next) {
+    try {
+      const { email, password } = req.body;
+      if (!password){
+        throw {name: 'PASSWORD_REQUEST'}
+      } else if (!email){
+        throw {name: 'EMAIL_REQUEST'}
+      }
+
+      const foundUser = await User.findOne({ where: { email } });
+      if (!foundUser) {
+        throw { name: "invalid_credentials" };
+      }
+
+      const isValid = comparePassword(password, foundUser.password);
+      if (!isValid) {
+        throw { name: "invalid_credentials" };
+      }
+
+      let payload = { id: foundUser.id, role: foundUser.role };
+      const access_token = signToken(payload);
+
+      req.headers = { access_token };
+
+      res.status(200).json({
+        access_token,
+        user: {
+          id: foundUser.id,
+          email: foundUser.email,
+          role: foundUser.role,
+        },
+      });
+    } catch (err) {
+      next(err);
     }
   }
 }
--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,20 +1,28 @@
 function errorHandler(err, req, res, next) {
-    let status = 500;
-    let message = "Internal server error";
-  
-    switch (err.name) {
-      case 'SequelizeValidationError':
-      case 'SequelizeUniqueConstraintError':
-          status = 201
-          message = err.errors[0].message
-        break;
-      
-      default:
-        break;
-    }
-    res.status(status).json({ message });
+  let status = 500;
+  let message = "Internal server error";
+
+  switch (err.name) {
+    case "SequelizeValidationError":
+    case "SequelizeUniqueConstraintError":
+      status = 201;
+      message = err.errors[0].message;
+      break;
+    case "PASSWORD_REQUEST":
+      status = 400;
+      message = "Password is required";
+      break;
+    case "EMAIL_REQUEST":
+      status = 400;
+      message = "Email is required";
+      break;
+    case "invalid_credentials":
+      status = 401;
+      message = "Invalid email/password";
+    default:
+      break;
   }
-  
-  
-  module.exports = errorHandler
-  
+  res.status(status).json({ message });
+}
+
+module.exports = errorHandler;
