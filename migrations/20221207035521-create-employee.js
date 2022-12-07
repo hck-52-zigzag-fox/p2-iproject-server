afb3f6d3a2b18dce8c6ddc1badf264ec09961101
atrsxvr <atrasx@gmail.com>
2022-12-07 12:42:32 +0700

    setup database

---
+++ migrations/20221207035521-create-employee.js
@@ -0,0 +1,53 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Employees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      age: {
+        type: Sequelize.INTEGER
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      experience: {
+        type: Sequelize.INTEGER
+      },
+      education: {
+        type: Sequelize.STRING
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      CategoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Categories',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Employees');
+  }
+};
---
+++ models/employee.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Employee extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Employee.init({
+    name: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+    gender: DataTypes.STRING,
+    experience: DataTypes.INTEGER,
+    education: DataTypes.STRING,
+    imageUrl: DataTypes.STRING,
+    status: DataTypes.STRING,
+    CategoryId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Employee',
+  });
+  return Employee;
+};
