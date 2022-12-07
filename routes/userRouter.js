bfe2fc7be199562497128bf1ebfb4d9615d5c43b
januaricks <muhammadjanu30@gmail.com>
2022-12-07 16:59:01 +0700

    add feature nodemailer

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.post("/register", UserController.handleRegister);
 router.post("/login", UserController.handleLogin);
+router.post("/googleLogin", UserController.handleGoogleLogin);
 
 module.exports = router;
