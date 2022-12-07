13c9a68342cc68b4e6f082a73b8ff5fe25c1414e
januaricks <muhammadjanu30@gmail.com>
2022-12-07 16:11:46 +0700

    add login google

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,10 @@
+const { OAuth2Client } = require("google-auth-library");
 const { comparePassword } = require("../helpers/password");
 const { createToken } = require("../helpers/token");
 const { User } = require("../models");
+
+GOOGLE_CLIENT_ID =
+  "839583652286-mimifttp9jfflgfvthhpk6bm3fbrt0sr.apps.googleusercontent.com";
 
 class UserController {
   static async handleRegister(req, res, next) {
@@ -38,7 +42,48 @@ class UserController {
       const payload = { id: foundUser.id };
       const access_token = createToken(payload);
 
-      res.status(200).json({ access_token });
+      res.status(200).json({
+        access_token,
+        user: {
+          id: foundUser.id,
+          email: foundUser.email,
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+  static async handleGoogleLogin(req, res, next) {
+    try {
+      const token = req.headers.google_token;
+      const CLIENT_ID = GOOGLE_CLIENT_ID;
+
+      const client = new OAuth2Client(CLIENT_ID);
+      const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,
+      });
+      const googlePayload = ticket.getPayload();
+
+      const [user, created] = await User.findOrCreate({
+        where: { email: googlePayload.email },
+        defaults: {
+          email: googlePayload.email,
+          password: "dariGoogleNich",
+        },
+        hooks: false,
+      });
+
+      let payload = { id: user.id };
+      const access_token = createToken(payload);
+
+      res.status(200).json({
+        access_token,
+        user: {
+          id: user.id,
+          email: user.email,
+        },
+      });
     } catch (err) {
       next(err);
     }
