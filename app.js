67a88ccd3f2cc462c0ee8360ae74b054d211b84e
januaricks <muhammadjanu30@gmail.com>
2022-12-07 13:44:10 +0700

    setup router user

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const cors = require("cors");
 const express = require("express");
 const { errorHandler } = require("./middlewares/errorHandling");
+const router = require("./routes");
 const app = express();
 const port = 9090;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(router);
 app.use(errorHandler);
 
 app.listen(port, () => {
