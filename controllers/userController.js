f1fefb23e719af4467efa0203e7d1cddf319c900
januaricks <muhammadjanu30@gmail.com>
2022-12-07 13:54:42 +0700

    add feature register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,21 @@
 const { User } = require("../models");
 
 class UserController {
-  static async register(req, res, next) {}
+  static async register(req, res, next) {
+    try {
+      const { email, password, phoneNumber, address } = req.body;
+
+      const newUser = await User.create({
+        email,
+        password,
+        phoneNumber,
+        address,
+      });
+      res.status(200).json({ id: newUser.id, email: newUser.email });
+    } catch (err) {
+      next(err);
+    }
+  }
 }
 
 module.exports = UserController;
--- middlewares/errorHandling.js
+++ middlewares/errorHandling.js
@@ -2,6 +2,14 @@ const errorHandler = (err, req, res, next) => {
   let status = 500;
   let message = "Internal Server Error";
 
+  if (
+    err.name === "SequelizeValidationError" ||
+    err.name === "SequelizeUniqueConstraintError"
+  ) {
+    status = 400;
+    message = err.errors[0].message;
+  }
+
   return res.status(status).json({ message });
 };
 
